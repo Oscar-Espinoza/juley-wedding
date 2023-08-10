bcf1d1da8b33c9c55ef7c2d5fcb2dd2f78f20097
Oscar-Espinoza <oscardavid2094@gmail.com>
2023-08-09 23:20:54 -0300

    worked in section one, flowers and date added

---
+++ components/Date.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="date-container">
+    <span class="date">15 de Noviembre del 2023</span>
+  </div>
+</template>
+
+<style scoped>
+  .date-container {
+    width: 100%;
+    text-align: center;
+  }
+
+  .date {
+    font-family: var(--fuente-2);
+    font-weight: var(--weight-font-regular);
+    color: var(--color-1);
+    display: table;
+    white-space: nowrap;
+    width: 100%;
+    font-size: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .date::after, .date::before {
+    border-top: 1px solid var(--color-2);
+    content: '';
+    width: 10%;
+    height: 1px;
+  }
+
+</style>
---
+++ components/TopSection.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="invite-section bg-pink-img">
+    <div class="flowers-container">
+      <img src="/assets/flower-left.webp" alt="left-flower">
+      <img src="/assets/flower-middle.webp" alt="middle-flower">
+      <img src="/assets/flower-right.webp" alt="right-flower">
+    </div>
+    <Date />
+  </section>
+  
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  .bg-pink-img {
+    background: url("/assets/fondo-top.webp");
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .flowers-container {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    width: 260px;
+    height: 108px;
+  }
+
+  .flowers-container img {
+    width: 140px;
+    height: 140px;
+    object-fit: contain;
+    position: absolute;
+    top: -40px;
+  }
+
+  .flowers-container img:nth-child(1) {
+    left: 0;
+  }
+
+  .flowers-container img:nth-last-child(1) {
+    right: 0;
+  }
+</style>
