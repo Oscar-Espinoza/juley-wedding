cb5efdbafa064d6a20cd9fe6951e84a3607d32cf
Oscar-Espinoza <oscardavid2094@gmail.com>
2023-08-29 22:35:12 -0300

    photos updated

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -2,12 +2,15 @@
   <div class="container">
     <h3 class="title">Retratos de Nuestro Amor</h3>
     <p class="text">Un minuto, un segundo, un unstante que queda en la eternidad</p>
-    <Swiper :modules="[EffectCube, Pagination]" effect="cube" :grabCursor="true" :loop="true" :cubeEffect="{
-      shadow: true,
-      slideShadows: true,
-      shadowOffset: 20,
-      shadowScale: 0.94,
-    }" :pagination="true">
+    <Swiper :loop="true" :spaceBetween="10" :thumbs="{ swiper: thumbsSwiper }" :modules="[FreeMode, Navigation, Thumbs]"
+      class="mySwiper2">
+      <SwiperSlide v-for="photo in photos">
+        <img :src="photo" alt="carousel-img">
+      </SwiperSlide>
+    </Swiper>
+
+    <Swiper @swiper="setThumbsSwiper" :spaceBetween="10" :slidesPerView="4" :freeMode="true" :watchSlidesProgress="true"
+      :modules="[FreeMode, Navigation, Thumbs]" class="mySwiper">
       <SwiperSlide v-for="photo in photos">
         <img :src="photo" alt="carousel-img">
       </SwiperSlide>
@@ -16,8 +19,15 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { Swiper, SwiperSlide } from 'swiper/vue'
-import { EffectCube, Pagination } from 'swiper/modules';
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
+
+const thumbsSwiper = ref(null);
+
+const setThumbsSwiper = (swiper) => {
+  thumbsSwiper.value = swiper;
+}
 
 const photos = [
   '/assets/photos/photo_1.webp',
@@ -29,10 +39,10 @@ const photos = [
   '/assets/photos/photo_7.webp',
   '/assets/photos/photo_8.webp',
 ]
-
-import 'swiper/css/effect-cube';
-import 'swiper/css/pagination';
-import 'swiper/css'
+import 'swiper/css';
+import "swiper/css/free-mode"
+import "swiper/css/navigation"
+import "swiper/css/thumbs"
 </script>
 
 <style scoped>
@@ -63,16 +73,22 @@ import 'swiper/css'
 }
 
 .swiper {
-  max-width: 400px;
-  width: calc(100% - 50px);
-  height: 350px;
-  overflow: visible;
-
+  max-width: 500px;
+  width: calc(100% - 40px);
 }
 
 .swiper-slide img {
+  display: block;
   width: 100%;
   height: 100%;
-  border-radius: 40px;
+  object-fit: cover;
+}
+
+.mySwiper .swiper-slide {
+  opacity: 0.6;
+}
+
+.mySwiper .swiper-slide-thumb-active {
+  opacity: 1;
 }
 </style>
